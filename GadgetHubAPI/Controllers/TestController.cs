74fb3f8fba800327d343eb3fa809dff5901969fe
harshawije97 <harshawije99@gmail.com>
2025-10-19 19:11:31 +0530

    Add project files.

---
+++ GadgetHubAPI/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GadgetHubAPI.Controllers
+{
+    [ApiController]
+    [Route("v1/api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
